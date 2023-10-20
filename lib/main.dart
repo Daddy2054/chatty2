e088286f64ef1a905ce3dc17aabdb568fb2eaa48
Jean <acalto2@gmail.com>
2023-10-20 11:35:45 +0200

    16. Inject dependencies globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'package:chatty/global.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'common/routes/routes.dart';
+import 'global.dart';
 
 Future<void> main() async {
   await Global.init();
