8f8a318134be60f0cdb53363e61001d6d308f457
Jean <acalto2@gmail.com>
2023-10-20 14:25:32 +0200

    20. Sign in module and page logo

--- lib/pages/frame/sign_in/view.dart
+++ lib/pages/frame/sign_in/view.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
+import '../../../common/values/values.dart';
 import 'index.dart';
 
 class SignInPage extends GetView<SignInController> {
   const SignInPage({super.key});
 
+  Widget _buildLogo() {
+    return Container(
+      margin: EdgeInsets.only(
+        top: 100.h,
+        bottom: 80.h,
+      ),
+      child: Text(
+        'Chatty .',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontWeight: FontWeight.bold,
+          fontSize: 34.sp,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: AppColors.primarySecondaryBackground,
       body: Center(
-        child: Text('sign in page'),
+        child: Column(
+          children: [
+            _buildLogo(),
+          ],
+        ),
       ),
     );
   }
