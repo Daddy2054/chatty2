510ea5350d46a7b672391be2f9ab6585042b4f8f
Jean <acalto2@gmail.com>
2023-10-20 10:46:49 +0200

    13. Understanding the middleware

--- lib/common/middlewares/router_auth.dart
+++ lib/common/middlewares/router_auth.dart
@@ -4,9 +4,9 @@ import 'package:get/get.dart';
 import '../routes/routes.dart';
 import '../store/store.dart';
 
-/// 检查是否登录
+///Check if you are logged in
 class RouteAuthMiddleware extends GetMiddleware {
-  // priority 数字小优先级高
+  // priority Smaller numbers have higher priority
   @override
   int? priority = 0;
 
@@ -14,12 +14,19 @@ class RouteAuthMiddleware extends GetMiddleware {
 
   @override
   RouteSettings? redirect(String? route) {
-    if (UserStore.to.isLogin || route == AppRoutes.SIGN_IN || route == AppRoutes.INITIAL) {
+    if (UserStore.to.isLogin ||
+        route == AppRoutes.SIGN_IN ||
+        route == AppRoutes.INITIAL) {
       return null;
     } else {
       Future.delayed(
-          Duration(seconds: 2), () => Get.snackbar("Tips", "Login expired, please login again!"));
-      return RouteSettings(name: AppRoutes.SIGN_IN);
+        const Duration(seconds: 2),
+        () => Get.snackbar(
+          "Tips",
+          "Login expired, please login again!",
+        ),
+      );
+      return const RouteSettings(name: AppRoutes.SIGN_IN);
     }
   }
 }
