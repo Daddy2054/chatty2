e56de88074a2e264021588dceb7e841c448ac2aa
Jean <acalto2@gmail.com>
2023-10-26 20:31:57 +0200

    78. Contact module and test API with Postman

--- lib/common/store/user.dart
+++ lib/common/store/user.dart
@@ -21,12 +21,13 @@ class UserStore extends GetxController {
   UserItem get profile => _profile.value;
   bool get hasToken => token.isNotEmpty;
   //set setIsLogin(login) => _isLogin.value = login;
-  
+
   @override
   void onInit() {
     super.onInit();
     token = StorageService.to.getString(STORAGE_USER_TOKEN_KEY);
     var profileOffline = StorageService.to.getString(STORAGE_USER_PROFILE_KEY);
+    print(profileOffline);
     if (profileOffline.isNotEmpty) {
       _isLogin.value = true;
       _profile(
