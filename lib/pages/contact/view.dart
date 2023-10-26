8e57bfcf0d77a8774df037d6a285398be1332311
Jean <acalto2@gmail.com>
2023-10-26 20:00:29 +0200

    77. Contact module and app bar

--- lib/pages/contact/view.dart
+++ lib/pages/contact/view.dart
@@ -4,19 +4,33 @@ import 'package:get/get.dart';
 
 import '../../common/values/colors.dart';
 import 'controller.dart';
+import 'widgets/contact_list.dart';
 
 class ContactPage extends GetView<ContactController> {
   const ContactPage({super.key});
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text(
+        'Contact',
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontSize: 16.sp,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryElement,
+      //   backgroundColor: AppColors.primaryElement,
+      appBar: _buildAppBar(),
       body: SizedBox(
         width: 360.w,
         height: 780.h,
         child: const Center(
-          child: Text('Contact hi!'),
+          child: ContactList(),
         ),
       ),
     );
