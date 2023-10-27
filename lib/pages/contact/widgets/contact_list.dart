782888e8bb943bb93ba2f944226ac46f28656e3f
Jean <acalto2@gmail.com>
2023-10-27 20:40:26 +0200

    84. Contact module, flutter load data and print p6

--- lib/pages/contact/widgets/contact_list.dart
+++ lib/pages/contact/widgets/contact_list.dart
@@ -1,14 +1,37 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-class ContactList extends StatelessWidget {
+import '../controller.dart';
+
+class ContactList extends GetView<ContactController> {
   const ContactList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('Contact Page'),
-      ),
-    );
+    return Obx(() {
+      return CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 0.w,
+              vertical: 0.h,
+            ),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  var item = controller.state.contactList[index];
+                  if (kDebugMode) {
+                    print(item.name);
+                  }
+                },
+                childCount: controller.state.contactList.length,
+              ),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
