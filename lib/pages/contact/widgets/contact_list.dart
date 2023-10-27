ed19e2d1d5486b9de25057962307404a212164bf
Jean <acalto2@gmail.com>
2023-10-27 21:17:39 +0200

    85. Contact module, show users profile photo p7

--- lib/pages/contact/widgets/contact_list.dart
+++ lib/pages/contact/widgets/contact_list.dart
@@ -1,12 +1,68 @@
-import 'package:flutter/foundation.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
+import '../../../common/entities/contact.dart';
+import '../../../common/values/colors.dart';
 import '../controller.dart';
 
 class ContactList extends GetView<ContactController> {
   const ContactList({super.key});
+
+  Widget _buildListItem(ContactItem item) {
+    return Container(
+      padding: EdgeInsets.only(top: 10.h),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1,
+            color: AppColors.primarySecondaryBackground,
+          ),
+        ),
+      ),
+      child: InkWell(
+        onTap: () {},
+        child: Row(
+          children: [
+            Container(
+              width: 44.w,
+              height: 44.h,
+              decoration: BoxDecoration(
+                color: AppColors.primarySecondaryBackground,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(22.w),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.1),
+                    spreadRadius: 1,
+                    blurRadius: 2,
+                    offset: const Offset(0, 1),
+                  ),
+                ],
+              ),
+              child: CachedNetworkImage(
+                imageUrl: item.avatar!,
+                height: 44.w,
+                width: 44.w,
+                imageBuilder: (context, imageProvider) => Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(22.w),
+                    ),
+                    image: DecorationImage(
+                      image: imageProvider,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +71,14 @@ class ContactList extends GetView<ContactController> {
         slivers: [
           SliverPadding(
             padding: EdgeInsets.symmetric(
-              horizontal: 0.w,
+              horizontal: 20.w,
               vertical: 0.h,
             ),
             sliver: SliverList(
               delegate: SliverChildBuilderDelegate(
                 (context, index) {
                   var item = controller.state.contactList[index];
-                  if (kDebugMode) {
-                    print(item.name);
-                  }
+                  return _buildListItem(item);
                 },
                 childCount: controller.state.contactList.length,
               ),
