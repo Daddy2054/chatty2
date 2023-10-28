85d315e4d65c2ceb8dec8abefb4cb41da51e76e8
Jean <acalto2@gmail.com>
2023-10-28 22:25:30 +0200

    92b. issue getting other users solved

--- lib/pages/contact/controller.dart
+++ lib/pages/contact/controller.dart
@@ -24,86 +24,7 @@ class ContactController extends GetxController {
     asyncLoadAllData();
   }
 
-  // Future<void> goChat(ContactItem contactItem) async {
-  //   var from_messages = await db
-  //       .collection('message')
-  //       .withConverter(
-  //         fromFirestore: Msg.fromFirestore,
-  //         toFirestore: (Msg msg, options) => msg.toFirestore(),
-  //       )
-  //       .where(
-  //         'from_token',
-  //         isEqualTo: token,
-  //       )
-  //       .where(
-  //         'to_token',
-  //         isEqualTo: contactItem.token,
-  //       )
-  //       .get();
-  //   if (kDebugMode) {
-  //   print('...from_messages ${from_messages.docs.isNotEmpty}');
-  //   }
-
-  //   var to_messages = await db
-  //       .collection('message')
-  //       .withConverter(
-  //         fromFirestore: Msg.fromFirestore,
-  //         toFirestore: (Msg msg, options) => msg.toFirestore(),
-  //       )
-  //       .where(
-  //         'from_token',
-  //         isEqualTo: contactItem.token,
-  //       )
-  //       .where(
-  //         'to_token',
-  //         isEqualTo: token,
-  //       )
-  //       .get();
-  //   if (kDebugMode) {
-  //   print('...to_messages ${to_messages.docs.isNotEmpty}');
-  //   }
-
-  //   if (from_messages.docs.isEmpty && to_messages.docs.isEmpty) {
-  //     var profile = UserStore.to.profile;
-  //     var msgdata = Msg(
-  //       from_token: profile.token,
-  //       to_token: contactItem.token,
-  //       from_name: profile.name,
-  //       to_name: contactItem.name,
-  //       from_avatar: profile.avatar,
-  //       to_avatar: contactItem.name,
-  //       from_online: profile.online,
-  //       to_online: contactItem.online,
-  //       last_msg: '',
-  //       last_time: Timestamp.now(),
-  //       msg_num: 0,
-  //     );
-  //     var doc_id = await db
-  //         .collection('message')
-  //         .withConverter(
-  //             fromFirestore: Msg.fromFirestore,
-  //             toFirestore: (Msg msg, options) => msg.toFirestore())
-  //         .add(msgdata);
-
-  //     // Get.offAllNamed(
-  //     //   '/chat',
-  //     //   parameters: {
-  //     //     'doc_id': doc_id.id,
-  //     //     'to_token': contactItem.token ?? '',
-  //     //     'to_name': contactItem.name ?? '',
-  //     //     'to_avatar': contactItem.online.toString(),
-  //     //   },
-  //     // );
-  //     if (kDebugMode) {
-  //       print('...creating new document and adding user info done.....');
-  //     }
-  //   } else {
-  //     if (kDebugMode) {
-  //       print('...users are older...');
-  //     }
-  //   }
-  // }
-
+ 
  Future<void> goChat(ContactItem contactItem) async {
     var from_messages = await db
         .collection('message')
@@ -175,7 +96,7 @@ class ContactController extends GetxController {
     state.contactList.clear();
     var result = await ContactAPI.post_contact();
     if (kDebugMode) {
-      print(result.data!);
+      print('$asyncLoadAllData: $result.data!');
     }
     if (result.code == 0) {
       state.contactList.addAll(result.data!);
