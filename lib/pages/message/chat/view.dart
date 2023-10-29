8efa6be911b5fc92a791bb825b3dd78752315c49
Jean <acalto2@gmail.com>
2023-10-29 12:48:51 +0100

    94. Chat module and build user photo at the top p2

--- lib/pages/message/chat/view.dart
+++ lib/pages/message/chat/view.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
@@ -10,19 +11,47 @@ class ChatPage extends GetView<ChatController> {
 
   AppBar _buildAppBar() {
     return AppBar(
-      title: Obx(() {
-        return Text(
-          '${controller.state.to_name}',
-          overflow: TextOverflow.clip,
-          maxLines: 1,
-          style: TextStyle(
-            fontFamily: 'Avenir',
-            fontWeight: FontWeight.bold,
-            color: AppColors.primaryText,
-            fontSize: 16.sp,
+      title: Obx(
+        () {
+          return Text(
+            '${controller.state.to_name}',
+            overflow: TextOverflow.clip,
+            maxLines: 1,
+            style: TextStyle(
+              fontFamily: 'Avenir',
+              fontWeight: FontWeight.bold,
+              color: AppColors.primaryText,
+              fontSize: 16.sp,
+            ),
+          );
+        },
+      ),
+      actions: [
+        Container(
+          margin: EdgeInsets.only(right: 20.w),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              SizedBox(
+                width: 44.w,
+                height: 44.h,
+                child: CachedNetworkImage(
+                  imageUrl: controller.state.to_avatar.value,
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(22.w),
+                      image: DecorationImage(image: imageProvider),
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => const Image(
+                    image: AssetImage('assets/images/account_header.png'),
+                  ),
+                ),
+              ),
+            ],
           ),
-        );
-      }),
+        ),
+      ],
     );
   }
 
