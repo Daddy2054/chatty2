5d95eaea9970dc98f515bc72b8067eef92f757b2
Jean <acalto2@gmail.com>
2023-10-31 19:53:19 +0100

    152. Chat: send message create message object p2

--- lib/pages/message/chat/controller.dart
+++ lib/pages/message/chat/controller.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: non_constant_identifier_names
 
+import 'package:chatty/common/store/user.dart';
+import 'package:chatty/common/widgets/widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../common/entities/entities.dart';
 import '../../../common/routes/routes.dart';
 import 'index.dart';
 
@@ -14,6 +18,8 @@ class ChatController extends GetxController {
 
   late String doc_id;
   final myInputController = TextEditingController();
+  //get the user or sender's token
+  final token = UserStore.to.profile.token;
 
   void goMore() {
     state.more_status.value = !state.more_status.value;
@@ -45,6 +51,17 @@ class ChatController extends GetxController {
   }
 
   void sendMessage() {
-    print('you tapped me');
+    String sendContent = myInputController.text;
+    print('...$sendContent...');
+    if (sendContent.isEmpty) {
+      toastInfo(msg: 'content is empty');
+    }
+
+    Msgcontent(
+      token: token,
+      content: sendContent,
+      type: 'text',
+      addtime: Timestamp.now(),
+    );
   }
 }
