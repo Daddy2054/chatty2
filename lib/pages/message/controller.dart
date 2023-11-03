0afb3dede1a804b03d13a875f0e3a55269d7f109
Jean <acalto2@gmail.com>
2023-11-03 21:05:46 +0100

    193. Message screen and load messages part 1

--- lib/pages/message/controller.dart
+++ lib/pages/message/controller.dart
@@ -1,4 +1,5 @@
 import 'package:chatty/common/entities/base.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,7 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
 
 import '../../common/apis/apis.dart';
+import '../../common/entities/entities.dart';
 import '../../common/routes/routes.dart';
 import '../../common/store/store.dart';
 import 'state.dart';
@@ -13,6 +15,8 @@ import 'state.dart';
 class MessageController extends GetxController {
   MessageController();
   final state = MessageState();
+  final db = FirebaseFirestore.instance;
+  final token = UserStore.to.profile.token;
 
   Future<void> goProfile() async {
     await Get.toNamed(
@@ -28,10 +32,81 @@ class MessageController extends GetxController {
         dismissOnTap: true);
     state.tabStatus.value = !state.tabStatus.value;
     if (state.tabStatus.value) {
-      //  asyncLoadMsgData();
+       asyncLoadMsgData();
     } else {}
 
     EasyLoading.dismiss();
+  }
+
+  Future<void> asyncLoadMsgData() async {
+    // var token = UserStore.to.profile.token;
+    var fromMessages = await db
+        .collection("message")
+        .withConverter(
+            fromFirestore: Msg.fromFirestore,
+            toFirestore: (Msg msg, options) => msg.toFirestore())
+        .where("from_token", isEqualTo: token)
+        .get();
+
+    if (kDebugMode) {
+      print(fromMessages.docs.length);
+    }
+    var toMessages = await db
+        .collection("message")
+        .withConverter(
+            fromFirestore: Msg.fromFirestore,
+            toFirestore: (Msg msg, options) => msg.toFirestore())
+        .where("to_token", isEqualTo: token)
+        .get();
+    if (kDebugMode) {
+      print(toMessages.docs.length);
+    }
+
+    state.msgList.clear();
+
+    if (fromMessages.docs.isNotEmpty) {
+      await addMessage(fromMessages.docs);
+    }
+
+    if (toMessages.docs.isNotEmpty) {
+      await addMessage(toMessages.docs);
+    }
+    state.msgList.sort((a, b) {
+      if (b.last_time == null) {
+        return 0;
+      }
+      if (a.last_time == null) {
+        return 0;
+      }
+      return b.last_time!.compareTo(a.last_time!);
+    });
+  }
+
+  addMessage(List<QueryDocumentSnapshot<Msg>> data) {
+    for (var element in data) {
+      var item = element.data();
+      Message message = Message();
+      //saves the commom proterties
+      message.doc_id = element.id;
+      message.last_time = item.last_time;
+
+      message.msg_num = item.msg_num;
+      message.last_msg = item.last_msg;
+      if (item.from_token == token) {
+        message.name = item.to_name;
+        message.avatar = item.to_avatar;
+        message.token = item.to_token;
+        message.online = item.to_online;
+        message.msg_num = item.to_msg_num ?? 0;
+      } else {
+        message.name = item.from_name;
+        message.avatar = item.from_avatar;
+        message.token = item.from_token;
+        message.online = item.from_online;
+        message.msg_num = item.from_msg_num ?? 0;
+      }
+      state.msgList.add(message);
+    }
   }
 
   @override
--- lib/pages/message/state.dart
+++ lib/pages/message/state.dart
@@ -6,4 +6,5 @@ class MessageState {
   // ignore: non_constant_identifier_names
   var head_detail = UserItem().obs;
   RxBool tabStatus = true.obs;
+  RxList<Message> msgList = <Message>[].obs;
 }
